02b3fa599ac9d26afeb91118d83dde5e40537229
neiths <thienkute01022002@gmail.com>
2026-08-09 18:09:51 +0700

    Gradio: queue enabled for streaming

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -286,8 +286,8 @@ def main():
 
     interface = create_gradio_interface()
 
-    # Launch the interface
-    interface.launch(
+    # Launch the interface with queue enabled for streaming generator support
+    interface.queue().launch(
         server_name="0.0.0.0",
         server_port=7861,  # Changed to avoid port conflict
         share=False,
