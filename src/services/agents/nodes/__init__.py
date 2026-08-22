bfa59483de6731f527c7d8b50f493db559be5176
thienhb <thienhb@tmtco.asia>
2026-08-22 10:52:38 +0700

    Agents: add node to generate answer from retrieval info

--- src/services/agents/nodes/__init__.py
+++ src/services/agents/nodes/__init__.py
@@ -1,7 +1,9 @@
 from .rewrite_query_node import ainvoke_rewrite_query_step
 from .retrieve_node import ainvoke_retrieve_step
+from .generate_answer_node import ainvoke_generate_answer_step
 
 __all__ = [
     "ainvoke_rewrite_query_step",
     "ainvoke_retrieve_step",
+    "ainvoke_generate_answer_step",
 ]
