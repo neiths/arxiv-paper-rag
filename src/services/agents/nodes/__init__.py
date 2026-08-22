3f78858e58ba49e0c93fdff925894b60d9e1b8ce
thienhb <thienhb@tmtco.asia>
2026-08-22 13:58:46 +0700

    Agents: add node to grade retrieval documents

--- src/services/agents/nodes/__init__.py
+++ src/services/agents/nodes/__init__.py
@@ -1,9 +1,11 @@
 from .rewrite_query_node import ainvoke_rewrite_query_step
 from .retrieve_node import ainvoke_retrieve_step
 from .generate_answer_node import ainvoke_generate_answer_step
+from .grade_documents_node import ainvoke_grade_documents_step
 
 __all__ = [
     "ainvoke_rewrite_query_step",
     "ainvoke_retrieve_step",
     "ainvoke_generate_answer_step",
+    "ainvoke_grade_documents_step",
 ]
